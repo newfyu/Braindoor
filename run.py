5aea56915add2ab37b8a2db26d0c65a85039eb0e
lhan <lhan@lhandeMacBook-Pro.local>
2023-03-11 12:47:27 +0800

    ui

--- run.py
+++ run.py
@@ -1,20 +1,28 @@
 import subprocess
 import rumps
+import webbrowser
 
 
 class BrainDoorBar(rumps.App):
     def __init__(self):
         super().__init__("🧠", quit_button=None)
         self.p = subprocess.Popen(['python', 'app.py'])
+        self.item_open = rumps.MenuItem("Open",callback=self.run_open)
         self.item_run = rumps.MenuItem("Run",callback=self.run_app)
         self.item_stop = rumps.MenuItem("Stop",callback=self.stop_app)
         self.item_quit = rumps.MenuItem("Quit",callback=self.quit)
+        self.menu.add(self.item_open)
         self.menu.add(self.item_stop)
         self.menu.add(self.item_quit)
+
+    def run_open(self,_):
+        url = "http://127.0.0.1:7860"
+        webbrowser.open(url)
 
     def run_app(self, _):
         self.p = subprocess.Popen(['python', 'app.py'])
         self.menu.clear()
+        self.menu.add(self.item_open)
         self.menu.add(self.item_stop)
         self.menu.add(self.item_quit)
         self.title = "🧠"
